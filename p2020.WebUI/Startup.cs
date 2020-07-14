72499ba3f8452b509aeb5e07b6489c6a171f82cd
Tanvi Desai <dtanvi1803@yahoo.co.in>
2020-07-14 16:09:51 +0530

    Solution and Project Setup

---
+++ p2020.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(p2020.WebUI.Startup))]
+namespace p2020.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
